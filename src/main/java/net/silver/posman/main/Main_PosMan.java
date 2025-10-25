b6c406cef6e77819a300c490543a621bccabaed4
icyhoty2k <icyhoty2k@gmail.com>
2025-10-26 02:17:19 +0300

    1

--- src/main/java/net/silver/posman/main/Main_PosMan.java
+++ src/main/java/net/silver/posman/main/Main_PosMan.java
@@ -4,16 +4,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.silver.posman.utils.DateTimeFormatters;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class Main_PosMan extends Application {
+  private static final String APP_TITLE = "PosMan";
+
+
+  static void main(String[] args) {
+    launch(args);
+  }
+
   @Override
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(Main_PosMan.class.getResource("v_PosMan.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello!");
+    Scene scene = new Scene(fxmlLoader.load());
     stage.setScene(scene);
     stage.show();
+    stage.setTitle(APP_TITLE + " - " + LocalDate.now().format(DateTimeFormatters.toCustomDate));
   }
 }
--- src/main/java/net/silver/posman/main/c_PosMan.java
+++ src/main/java/net/silver/posman/main/c_PosMan.java
@@ -1,14 +1,5 @@
 package net.silver.posman.main;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class c_PosMan {
-  @FXML
-  private Label welcomeText;
 
-  @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
-  }
 }
