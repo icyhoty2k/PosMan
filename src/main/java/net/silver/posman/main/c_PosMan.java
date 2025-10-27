1bb31eb9099440338379bac87517825b5352bb0a
icyhoty2k <icyhoty2k@gmail.com>
2025-10-27 03:48:16 +0200

    1

--- src/main/java/net/silver/posman/main/c_PosMan.java
+++ src/main/java/net/silver/posman/main/c_PosMan.java
@@ -1,16 +1,15 @@
 package net.silver.posman.main;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class c_PosMan {
 
-  @javafx.fxml.FXML private TextField test;
+  @FXML private TextField test;
 
-  @javafx.fxml.FXML public void testAction(ActionEvent actionEvent) {
+  @FXML public void testAction(ActionEvent actionEvent) {
 
     System.out.println("actionEvent = ");
-
-
   }
 }
