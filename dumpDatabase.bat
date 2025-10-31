90c43e8cafc3a21c82983101686775a93bfbdbdd
adminO <icyhoty2k@gmail.com>
2025-11-01 00:51:24 +0200

    1

--- dumpDatabase.bat
+++ dumpDatabase.bat
@@ -1,1 +1,9 @@
-mysqldump --host=localhost --port=3306 --default-character-set=utf8 --user=root -pqwe123 --protocol=tcp --single-transaction=TRUE --routines --events "pos_manager" > "dbDump/backup-%date%-%time::=-%.sql"
+@echo off
+
+setlocal enabledelayedexpansion
+
+:: Use PowerShell to get a locale-independent timestamp
+for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format \"yyyy_MM_dd_HH_mm_ss\""') do set timestamp=%%i
+
+echo Timestamp: !timestamp!
+mysqldump --host=localhost --port=3306 --default-character-set=utf8 --user=root -pqwe123 --protocol=tcp --single-transaction=TRUE --routines --events "pos_manager" > "dbDump/backup_!timestamp!.sql"
