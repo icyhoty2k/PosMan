798f3d1a82515da1f2bdd984ce84e603888d2405
adminO <icyhoty2k@gmail.com>
2025-11-01 01:08:03 +0200

    1

--- dumpDatabase.bat
+++ dumpDatabase.bat
@@ -4,6 +4,4 @@ setlocal enabledelayedexpansion
 
 :: Use PowerShell to get a locale-independent timestamp
 for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format \"yyyy_MM_dd_HH_mm_ss\""') do set timestamp=%%i
-
-echo Timestamp: !timestamp!
-mysqldump --host=localhost --port=3306 --default-character-set=utf8 --user=root -pqwe123 --protocol=tcp --single-transaction=TRUE --routines --events "pos_manager" > "dbDump/backup_!timestamp!.sql"
+mysqldump --defaults-extra-file=I:/10_JavaSources/PosMan/config.cnf --default-character-set=utf8 --protocol=tcp --single-transaction=TRUE --routines --events "pos_manager" > "dbDump/backup_!timestamp!.sql"
