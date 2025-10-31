59d50630b6cedb9dc609d63ba76f642a34a76e46
adminO <icyhoty2k@gmail.com>
2025-11-01 00:03:11 +0200

    1

--- dumpDatabase.bat
+++ dumpDatabase.bat
@@ -1,1 +1,1 @@
-mysqldump --host=localhost --port=3306 --default-character-set=utf8 --user=root -pqwe123 --protocol=tcp --single-transaction=TRUE --routines --events "pos_manager" >dbDump/backup-%date%-%time::=-%.sql
+mysqldump --host=localhost --port=3306 --default-character-set=utf8 --user=root -pqwe123 --protocol=tcp --single-transaction=TRUE --routines --events "pos_manager" > "dbDump/backup-%date%-%time::=-%.sql"
