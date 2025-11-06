a736ae9c9e5ef603c36238e218d035e32b5f0570
icyhoty2k <icyhoty2k@gmail.com>
2025-11-06 04:44:22 +0200

    1

--- src/main/java/net/silver/posman/main/C_PosMan.java
+++ src/main/java/net/silver/posman/main/C_PosMan.java
@@ -3,8 +3,6 @@ package net.silver.posman.main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
@@ -12,9 +10,6 @@ import net.silver.posman.utils.StageManager;
 
 public class C_PosMan {
 
-  @FXML private Label lblCurrentDate;
-  @FXML private Label lblCurrentTime;
-  @FXML private Button btnLogout;
   @FXML private TextField test;
   @FXML private GridPane gridPaneMain;
 
@@ -29,7 +24,7 @@ public class C_PosMan {
     System.out.println("actionEvent = ");
   }
 
-  @FXML public void btnLogoutOnAction(ActionEvent actionEvent) {
+  @Deprecated public void btnLogoutOnAction(ActionEvent actionEvent) {
     StageManager.loadLoginStage();
     StageManager.mainStage.close();
   }
