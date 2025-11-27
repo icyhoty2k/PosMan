9bd5313aeefbdb6b78ca0dc4a27bf83834b0185a
adminO <icyhoty2k@gmail.com>
2025-11-27 20:31:18 +0200

    1

--- Gui/src/main/java/module-info.java
+++ Gui/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module net.silver.gui {
   requires javafx.fxml;
   requires net.silver.logging;
 
+
   exports net.silver.gui.main;
   exports net.silver.gui.login;
   exports net.silver.gui.groups;
