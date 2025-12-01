ee659c31f6953f13d6890ac045f1861ed6d4c8e8
adminO <icyhoty2k@gmail.com>
2025-12-01 15:28:27 +0200

    1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.gradle.declarative.dsl.schema.FqName.Empty.packageName
 import org.gradle.jvm.toolchain.internal.AsdfInstallationSupplier
+
 
 plugins {
     kotlin("jvm") version "2.2.21"  // Enables Kotlin + Java compilation for buildSrc
+
 }
 
 repositories {
